c725605b81e0b20ed8f56858d4e69dabfdbb338e
taguchi-k <k.taguchi@st-ventures.jp>
2017-03-06 23:33:40 +0900

    Add SampleCode

--- ios-uikit-uiscrollview-demo/ViewController.swift
+++ ios-uikit-uiscrollview-demo/ViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+final class ViewController: UIViewController {
+    
+    @IBOutlet fileprivate weak var scrollView: UIScrollView!
+    @IBOutlet fileprivate weak var pageControl: UIPageControl!
+    
+    // pageControllタップ時のアクション
+    @IBAction private func didTapPageControl(_ sender: UIPageControl) {
+        // scrollViewの位置も変える
+        scrollView.contentOffset = CGPoint(x: scrollView.frame.size.width * CGFloat(sender.currentPage), y: 0)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+// MARK: - UIScrollViewDelegate
+
+extension ViewController: UIScrollViewDelegate {
+    
+    // スクロール停止時に呼び出される
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        // pageControllのページを更新する
+        pageControl.currentPage = Int(scrollView.contentOffset.x / scrollView.frame.maxX)
+    }
+}
