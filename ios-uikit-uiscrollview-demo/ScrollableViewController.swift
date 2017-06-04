8fba4bac470b28d876e429b7a91addb1ad4e70ee
y.okudera <mail@sample.com>
2017-06-05 01:04:01 +0900

    [Add] 縦スクロールする画面を追加

---
+++ ios-uikit-uiscrollview-demo/ScrollableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ScrollableViewController.swift
+//  ios-uikit-uiscrollview-demo
+//
+//  Created by OkuderaYuki on 2017/06/05.
+//  Copyright © 2017年 Kentao Taguchi. All rights reserved.
+//
+
+import UIKit
+
+/// 縦スクロール画面
+final class ScrollableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
